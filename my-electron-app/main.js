e248ca13532bd5e031c66ea0e90a306674d0a3ea
Ryan Hogan <Ryanoman2011@Hotmail.com>
2024-12-03 15:35:27 +0000

    Added function to check for uploaded files.

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,17 +1,37 @@
-const { app, BrowserWindow, Menu, globalShortcut } = require('electron/main')
+// Imports.
+const { app, BrowserWindow, Menu, globalShortcut, ipcMain } = require('electron/main')
 const path = require('path');
+const fs = require('fs');
 
+// Checking if you're a dev or on macOS.
 const isDev = !app.isPackaged
 const isMac = process.platform === 'darwin'
 
+// App dimensions.
 const appWidth = 800
 const appHeight = 600
+
+// Check for user uploads.
+function checkForUploads() {
+  const uploadDir = path.join(__dirname, 'user_uploads')
+  const files = fs.readdirSync(uploadDir)
+  
+  if(files.length === 0) {
+    return "No files found."
+  }
+  return files
+}
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: appWidth,
     height: appHeight,
-    icon: path.join(__dirname, 'assets', 'favicon.ico')
+    icon: path.join(__dirname, 'assets', 'favicon.ico'),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.setMinimumSize(appWidth / 2, appHeight / 2)
@@ -28,8 +48,13 @@ const createWindow = () => {
   }
 }
 
+ipcMain.handle('check-uploads', () => {
+  return checkForUploads()
+})
+
 app.whenReady().then(() => {
   createWindow()
+  checkForUploads()
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
