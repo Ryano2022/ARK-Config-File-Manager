5b6d2392d10428181603c83095a361ff8524d0cc
Ryan Hogan <Ryanoman2011@Hotmail.com>
2025-03-03 19:23:10 +0000

    Moved stat/attribute mapping to it's own file.

---
+++ my-electron-app/src/assets/data/stats.js
@@ -0,0 +1,25 @@
+export const STAT_MAPPING = {
+  0: { name: "Health", icon: "health.webp" },
+  1: { name: "Stamina", icon: "stamina.webp" },
+  2: { name: "Torpidity", icon: "torpidity.webp" },
+  3: { name: "Oxygen", icon: "oxygen.webp" },
+  4: { name: "Food", icon: "food.webp" },
+  5: { name: "Water", icon: "water.webp" },
+  6: { name: "Temperature", icon: "fortitude.webp", unused: true },
+  7: { name: "Weight", icon: "weight.webp" },
+  8: { name: "Melee Damage", icon: "melee_damage.webp" },
+  9: { name: "Movement Speed", icon: "movement_speed.webp" },
+  10: { name: "Fortitude", icon: "fortitude.webp" },
+  11: { name: "Crafting Speed", icon: "crafting_speed.webp" },
+};
+
+export const ATTRIBUTE_MAPPING = {
+  0: { name: "Generic Quality" },
+  1: { name: "Armour" },
+  2: { name: "Max Durability" },
+  3: { name: "Weapon Damage Percent" },
+  4: { name: "Weapon Clip Ammo" },
+  5: { name: "Hypothermal Insulation (Cold Resist)" },
+  6: { name: "Weight" },
+  7: { name: "Hyperthermal Insulation (Heat Resist)" },
+};
--- my-electron-app/src/pages/view_page/contentDisplayHandler.js
+++ my-electron-app/src/pages/view_page/contentDisplayHandler.js
@@ -3,35 +3,12 @@ import { parseConfig } from "./configFileParser.js";
 import { formatValue, addBooleanToggle, formatNumber } from "./tableFormatter.js";
 import { itemClasses as itemsASERaw } from "../../assets/data/evolved/itemClasses.js";
 import { engramEntries as engramsASERaw } from "../../assets/data/evolved/engramEntries.js";
+import { STAT_MAPPING, ATTRIBUTE_MAPPING } from "../../assets/data/evolved/attributeMappings.js";
 
 // ASE Stat icons were downloaded from https://ark.wiki.gg/wiki/Attributes.
 const ASE_STAT_ICONS = "../../assets/icons/stats/evolved/";
 
-const STAT_MAPPING = {
-  0: { name: "Health", icon: "health.webp" },
-  1: { name: "Stamina", icon: "stamina.webp" },
-  2: { name: "Torpidity", icon: "torpidity.webp" },
-  3: { name: "Oxygen", icon: "oxygen.webp" },
-  4: { name: "Food", icon: "food.webp" },
-  5: { name: "Water", icon: "water.webp" },
-  6: { name: "Temperature", icon: "fortitude.webp", unused: true },
-  7: { name: "Weight", icon: "weight.webp" },
-  8: { name: "Melee Damage", icon: "melee_damage.webp" },
-  9: { name: "Movement Speed", icon: "movement_speed.webp" },
-  10: { name: "Fortitude", icon: "fortitude.webp" },
-  11: { name: "Crafting Speed", icon: "crafting_speed.webp" },
-};
 
-const ATTRIBUTE_MAPPING = {
-  0: { name: "Generic Quality" },
-  1: { name: "Armour" },
-  2: { name: "Max Durability" },
-  3: { name: "Weapon Damage Percent" },
-  4: { name: "Weapon Clip Ammo" },
-  5: { name: "Hypothermal Insulation (Cold Resist)" },
-  6: { name: "Weight" },
-  7: { name: "Hyperthermal Insulation (Heat Resist)" },
-};
 
 const itemsASE = Object.fromEntries(Object.entries(itemsASERaw).map(([key, value]) => [key.toLowerCase(), value]));
 const engramsASE = Object.fromEntries(Object.entries(engramsASERaw).map(([key, value]) => [key.toLowerCase(), value]));
