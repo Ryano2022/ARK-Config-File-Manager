4b142614d046d0877e6f692f0b8504fb6b20aaf9
Ryan Hogan <Ryanoman2011@Hotmail.com>
2025-03-10 18:18:43 +0000

    Beginnings of Share page.

---
+++ my-electron-app/src/pages/share_page/share.js
@@ -0,0 +1,16 @@
+const fileList = document.getElementById("fileList");
+
+// Add a file to Firestore.
+function addFileToFirestore() {}
+
+// Remove a file from Firestore.
+function removeFileFromFirestore() {}
+
+// Retrieve all the files from Firestore.
+function retrieveFilesFromFirestore() {}
+
+// Add a file to the list of files.
+function addFileToList() {}
+
+// Remove a file from the list of files.
+function removeFileFromList() {}
